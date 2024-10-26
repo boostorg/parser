f3ea57f4a25e71f3abe5b1c4ad3ab0c8d3c4cf4b
Zach Laine <whatwasthataddress@gmail.com>
2024-10-26 15:16:55 -0500

    Initial sketch of a CTRE-based lexer.

---
+++ include/boost/parser/lexer.hpp
@@ -0,0 +1,193 @@
+// Copyright (C) 2024 T. Zachary Laine
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+#ifndef BOOST_PARSER_LEXER_HPP
+#define BOOST_PARSER_LEXER_HPP
+
+#include <string_view>
+#include <type_traits>
+
+
+namespace boost { namespace parser {
+
+    struct none;
+
+#if 1 // TODO
+    struct none
+    {};
+#endif
+
+    namespace detail {
+        enum class token_kind { string_view, long_long, double_ };
+    }
+
+    /** TODO */
+    template<typename CharType>
+    struct token
+    {
+        using char_type = CharType;
+        using string_view = std::basic_string_view<CharType>;
+
+        constexpr token() : id_(0), kind_(detail::token_kind::no_value) {}
+        constexpr token(int id, string_view value) :
+            id_(id), kind_(detail::token_kind::no_value)
+        {
+            sv_ = value;
+        }
+        constexpr token(int id, long long value) :
+            id_(id), kind_(detail::token_kind::long_long)
+        {
+            ll_ = value;
+        }
+        constexpr token(int id, double value) :
+            id(id), kind(detail::token_kind::double_)
+        {
+            d_ = value;
+        }
+
+        constexpr bool id() const { return id_; }
+
+        constexpr nool has_string_view() const
+        {
+            return kind_ == detail::token_kind::string_view;
+        }
+        constexpr string_view get_string_view() const
+        {
+            BOOST_PARSER_DEBUG_ASSERT(kind == detail::token_kind::string_view);
+            return match_range_;
+        }
+
+        constexpr bool has_long_long() const
+        {
+            return kind_ == detail::token_kind::long_long;
+        }
+        constexpr long long get_long_long() const
+        {
+            BOOST_PARSER_DEBUG_ASSERT(kind == detail::token_kind::long_long);
+            return value.ll;
+        }
+
+        constexpr bool has_double() const
+        {
+            return kind_ == detail::token_kind::double_;
+        }
+        constexpr double get_double() const
+        {
+            BOOST_PARSER_DEBUG_ASSERT(kind == detail::token_kind::double_);
+            return value.d;
+        }
+
+        constexpr bool operator==(token const & rhs) const
+        {
+            if (id != rhs.id || kind != rhs.kind)
+                return false;
+            if (has_value()) {
+                if (kind == detail::token_kind::long_long)
+                    return
+            }
+        }
+
+    private:
+        union value
+        {
+            string_view sv_;
+            long long ll_;
+            double d_;
+        };
+        int id_;
+        detail::token_kind kind_;
+    };
+
+    namespace detail {
+        template<typename T>
+        token_kind token_kind_for()
+        {
+            if constexpr (std::is_smae_v<T, none>) {
+                return token_kind::string_view;
+            } else if constexpr (std::is_smae_v<T, long long>) {
+                return token_kind::long_long;
+            } else if constexpr (std::is_smae_v<T, double>) {
+                return token_kind::double_;
+            } else {
+                static_assert(
+                    !std::is_same_v<T, T>,
+                    "The only valid types for the 'Value' template parameter "
+                    "to 'lexer_token_spec' are 'none', 'long long', and "
+                    "'double'.");
+            }
+        }
+    }
+
+    /** TODO */
+    template<auto & Regex, typename Value = none>
+    struct lexer_token_spec
+    {
+        static_assert(
+            std::is_same_v<Value, none> || std::is_same_v<Value, long long> ||
+            std::is_same_v<Value, double>);
+
+        using value_type = Value;
+
+        explicit lexer_token_spec(int id) : id(id) {}
+
+        static constexpr auto & regex = Regex;
+
+        int id;
+    };
+
+#ifdef BOOST_PARSER_DOXYGEN
+
+    /** TODO */
+    template<typename Regex>
+    struct lexer;
+
+#else
+
+    // TODO: Indicate in the docs that using the lexer implies using these
+    // allocating variables.
+    template<typename Regex, int Count>
+    struct lexer
+    {
+        Regex regex;
+        std::vector<int> ids; // TODO: reintroduce IDType.
+        std::vector<detail::token_kind> value_types;
+    };
+
+#endif
+
+    /** TODO */
+    template<typename T, typename... Ts>
+    auto make_lexer(T const & x, Ts const &... xs)
+    {
+        auto regex = (re<T::regex>() | ... | re<Ts::regex>());
+        return lexer<decltype(regex), sizeof...(xs) + 1>{
+            regex,
+            {detail::token_kind_for<typename T::value_type>(),
+             detail::token_kind_for<typename Ts::value_type>()...},
+            {T::id, Ts::id...}};
+    }
+
+    // TODO: Jus' a sketch.
+    void process_tokens(lexer<Regex, Count> lex, std::string_view input)
+    {
+        for (/*TODO while?*/) {
+            auto results = ctre::match<"^([a-z]++)|([0-9]++)$">(input);
+            if (!results) {
+                // TODO: Report error.
+            }
+            hl::fold_n<Count>(
+                std::string_view{},
+                [&](auto state, auto i) -> std::string_view {
+                    if (results.get<i.value>())
+                        return results.get<i.value>();
+                    else
+                        return state;
+                });
+        }
+    }
+
+}}
+
+#endif
---
+++ misc/boostify_ctre.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+# Copyright (c) 2024 T. Zachary Laine
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Adapts the single-header form of the CTRE library for use as a header in Boost; rewrites this given file in place.')
+parser.add_argument('source', type=str, help='The CTRE header to adapt.')
+args = parser.parse_args()
+
+lines = open(args.source, 'r').readlines()
+
+tmp_file_name = 'ctre_header.temp.hpp'
+
+tmp_file = open(tmp_file_name, 'w')
+
+tmp_file.write('''// Copyright (C) 2024 Hana Dusíková, T. Zachary Laine
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// Original work by Hana Dusíková; modified for Boost macro and namespace
+// conventions by T. Zachary Laine.
+
+''')
+
+for line in lines:
+    line = line
+    line = line.replace('CTRE', 'BOOST_PARSER_CTRE', 1)
+    line = line.replace('CTLL', 'BOOST_PARSER_CTLL', 1)
+    namespaces = ['ctll', 'ctre', 'detail', 'detail::tables', 'literals', 'test_literals', 'tables', 'uni', 'uni::detail']
+    for ns in namespaces:
+        line = line.replace(f'namespace {ns}', f'namespace boost::parser::{ns}')
+    tmp_file.write(line)
+    #print(line[:-1])
+
+os.rename(tmp_file_name, args.source)
